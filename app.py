762852171e4c1a19d708900ebdb6b64a323ca154
Felicitas Lock <lockfelicitas@aol.com>
2024-11-23 04:02:49 +0100

    #2 basic database setup in python backend

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# config database connection
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/user_management_db_hackatum'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# initialize db object
+db = SQLAlchemy(app)
 
 # Route for index page
 @app.route("/")
 def home():
+    print("nice")
     return render_template("index.html")
 
 # Entry Point
