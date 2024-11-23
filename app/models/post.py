4f8f767172db0eeb05ff25cb08b9314a985b525f
Felicitas Lock <lockfelicitas@aol.com>
2024-11-23 14:45:07 +0100

    fix post issue when creating new post

--- app/models/post.py
+++ app/models/post.py
@@ -1,38 +1,33 @@
 from datetime import datetime
-
 from . import db
-
 
 class Post(db.Model):
     __tablename__ = 'posts'
-
     post_id = db.Column(db.Integer, primary_key=True)
     orga_id = db.Column(db.Integer, db.ForeignKey('organizations.orga_id'), nullable=False)
     title = db.Column(db.String(255), nullable=False)
     content = db.Column(db.Text, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
-
     categories = db.relationship('Category', secondary='post_categories', back_populates='posts')
-
+    organization = db.relationship('Organization', back_populates='posts')
 
     def __init__(self, title, content, orga_id):
         self.title = title
         self.content = content
         self.orga_id = orga_id
 
+
     def match_with_users(self):
         from user import User
         from user_interest import UserInterest
         from category import Category
-        # i want to have all users that are interested in the category and send them a notification
         matching_users = db.session.query(User).join(
-            UserInterest, User.user_id == UserInterest.user_id  # link user with userinterest
+            UserInterest, User.user_id == UserInterest.user_id
         ).join(
-            Category, UserInterest.category_id == Category.category_id  # link userinterest with category
+            Category, UserInterest.category_id == Category.category_id
         ).filter(
             Category.category_id.in_([category.category_id for category in self.categories])
-            # Filter filter for category of post
         ).all()
 
         for user in matching_users:
